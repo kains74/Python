762ceced8f4f247177c672e14cf54983bce01f59
kains74 <124328562+kains74@users.noreply.github.com>
2023-02-23 16:39:50 +0500

    release 3

--- Python_1/task_1/task_2.py
+++ Python_1/task_1/task_2.py
@@ -1,4 +1,8 @@
-time = int(input("Введите время в секундах: "))
+while True:
+    time = int(input("Введите время в секундах: "))
+    if time > 0:
+        break
+    print("Время не может быть отрицательным!")
 mm_temp = time // 60
 hh = mm_temp // 60
 mm = mm_temp % 60
